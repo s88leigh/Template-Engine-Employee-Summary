72967f61bf4ebc24770ea785283db985947b585f
Sophie <sophie88leigh@gmail.com>
2019-12-06 21:55:44 -0500

    added hbs file

--- index.js
+++ index.js
@@ -1,70 +1,93 @@
-const fs = require("fs");
-const inquirer = require("inquirer");
-const jest = require("jest");
-
-const generateHTML = require("./generateHTML");
-const path = require("path");
+const fs = require('fs');
 const puppeteer = require('puppeteer');
-// const employee = require("./Employee.test");
-// const engineer = require("./Engineer.test");
-// const intern = require("./Intern.test");
-// const manager = require("./Manager.test");
+const hbs = require('handlebars');
+const path =require('path');
+//const data = require('./database.json');
 
-
-
-const questions = [
-  {
-    type: "list",
-    name: "name",
-    message: "Choose a manager.",
-    choices: ["Alice", "George", "Melissa", "Sam", "Paul"]
-  },
-  {
-    type: "list",
-    name: "name",
-    message: "Choose an engineer",
-    choices: ["Tim", "Jan", "Kelly", "Christina", "Jack"]
-  },
-  {
-    type: "list",
-    name: "name",
-    message: "Choose an intern",  
-    choices: ["Kathy", "Greg", "Lisa", "Jim"]
-  }
-];
-
-const compile = async function (filename, data) {
-  const filePath = path.join(process.cwd(), "myTeam.pdf");
+const compile = async function(templateName, data ) {
+  const filePath = path.join(process.cwd(), 'templates', `${templateName}.hbs`);
   const html = await fs.readFile(filePath, 'utf-8');
+  return hbs.compile(html)(data);
 };
 
-async function init() {
-
+(async function() {
   try {
-    const answers = await inquirer.prompt(questions);
-    console.log(answers)
-    if (!answers) { throw "No answers provided!" };
 
-    const HTML = generateHTML(data)
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
 
-    fs.writeFile("index.html", HTML, err => console.log(err));
+      const content = await compile('teamRoster', data);
 
+      await page.setContent(content);
+      await page.emulateMedia('screen');
+      await page.pdf({
+        path: 'myTeam.pdf'
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setContent(HTML);
-    // await page.emulateMedia("screen");
+      })
 
-    // Generates a PDF from the page content
-    await page.pdf({ path: 'myTeam.pdf' });
+      console.log('done');
+      await browser.close();
+      process.exit();
 
-    console.log('done');
-    await browser.close();
-    process.exit();
   } catch (error) {
-    console.log(error)
+    console.log ('our error', error);
   }
+})();
 
-}
 
-init();
+// const questions = [
+//   {
+//     type: "list",
+//     name: "name",
+//     message: "Choose a manager for your team.",
+//     choices: ["Alice", "George", "Melissa", "Sam", "Paul"]
+//   },
+//   {
+//     type: "list",
+//     name: "name",
+//     message: "Choose an engineer",
+//     choices: ["Tim", "Jan", "Kelly", "Christina", "Jack"]
+//   },
+//   {
+//     type: "list",
+//     name: "name",
+//     message: "Choose an intern for your team",  
+//     choices: ["Kathy", "Greg", "Lisa", "Jim"]
+//   }
+// ];
+
+// const compile = async function (filename, data) {
+//   const filePath = path.join(process.cwd(), "myTeam.pdf");
+//   const html = await fs.readFile(filePath, 'utf-8');
+// };
+
+// async function init() {
+
+//   try {
+//     const answers = await inquirer.prompt(questions);
+//     console.log(answers)
+//     if (!answers) { throw "No answers provided!" };
+
+//     const HTML = generateHTML(employees)
+
+//     fs.writeFile("index.html", HTML, err => console.log(err));
+
+
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.setContent(HTML);
+//     // await page.emulateMedia("screen");
+
+//     // Generates a PDF from the page content
+//     await page.pdf({ path: 'myTeam.pdf' });
+
+//     console.log('done');
+//     await browser.close();
+//     process.exit();
+//   } catch (error) {
+//     console.log(error)
+//   }
+
+// }
+
+// init();
