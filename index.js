322167c06175f3d59d038a5b5d9aa67d0c5c7941
Sophie <sophie88leigh@gmail.com>
2019-12-09 13:52:42 -0500

    add explanations to intern.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+//inquirer prompts user with questions
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+//jest runs tests
 const jest = require("jest");
 const generateHTML = require("./generateHTML")
-
+//writeFile will create html page
 const writeFileAsync = util.promisify(fs.writeFile);
 
 
@@ -123,7 +125,7 @@ async function init() {
       const answers = await promptUser();
 
       const html = generateHTML(answers);
-
+    //writeFile will creat html page with the answers
       await writeFileAsync("index.html", html);
 
       console.log("Successfully wrote to index.html");
